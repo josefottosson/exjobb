f390d588cd6d57a10e201dbb80536ff6ee597d1f
josefottosson <jo222ev@student.lnu.se>
2013-04-16 21:03:34 +0200

    first commit

---
+++ nodeJS/js/js.js
@@ -0,0 +1,8 @@
+var myApp = {
+
+  init: function () {
+    console.log('hello world');
+  }
+};
+
+window.onload = myApp.init;
---
+++ nodeJS/requestHandlers.js
@@ -0,0 +1,35 @@
+var querystring = require("querystring");
+
+function start(response) {
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response, request) {
+  console.log("Request handler 'upload' was called.");
+}
+
+function show(response) {
+  
+}
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
