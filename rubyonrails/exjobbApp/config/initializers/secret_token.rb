c68d1d6e21fa74726a5df54fc9629763ba131283
josefottosson <jo222ev@student.lnu.se>
2013-04-16 21:36:03 +0200

    rails commit

---
+++ rubyonrails/exjobbApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExjobbApp::Application.config.secret_token = '89c7dcd495b07f17ad57144a88a6478bf15c73581f150e99df6b423caccae41a9d1c21b5c3606629e6a027a68fb0558d99529d4547f7aec08be0d8fc642f14b5'
