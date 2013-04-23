502ab7416412f1648819fa8d32f56e01fd7e0502
josefottosson <jo222ev@student.lnu.se>
2013-04-23 20:01:36 +0200

    comments added

--- php/Connect.php
+++ php/Connect.php
@@ -1,11 +1,14 @@
 <?php
 	function ConnectToDb()
 	{
+		//Create new Mongo instance
 		$connection = new Mongo();
+		//Choose database
 		$db = $connection->selectDB('exjobb');
 		$dbInfo = Array();
 		$dbInfo[0] = $db;
 		$dbInfo[1] = $connection;
+		//Returns array with db object and connection object
 		return $dbInfo;
 	}
 
